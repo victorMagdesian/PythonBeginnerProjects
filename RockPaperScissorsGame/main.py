2c38334d251b82487ddd80411472c20c0547d39e
victorMagdesian <vicfemagdesian@gmail.com>
2022-11-21 14:12:55 -0300

    RockPaperScissorGame dir added + ttk basic window configuration made for rockPaperScissorGame

---
+++ RockPaperScissorsGame/main.py
@@ -0,0 +1,19 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+# colorsHex --------------------------------
+white = "#FFFFFF"  # white / branca
+black = "#333333"  # black / preta
+orange = "#fcc058"  # orange / laranja
+yellow = "#fff873"  # yellow / amarela
+green = "#34eb3d"   # green / verde
+red = "#e85151"   # red / vermelha
+background = "#3b3b3b"
+
+window = Tk()
+window.title("game")
+window.geometry("260x280")
+window.configure(bg=background)
+
+window.mainloop()

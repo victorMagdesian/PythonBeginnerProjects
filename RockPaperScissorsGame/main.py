db83599f8a49de45f67bc16d2f0f4894367d0b49
victorMagdesian <vicfemagdesian@gmail.com>
2022-11-21 14:32:33 -0300

    ttk window style added + frames created

--- RockPaperScissorsGame/main.py
+++ RockPaperScissorsGame/main.py
@@ -11,9 +11,20 @@ green = "#34eb3d"   # green / verde
 red = "#e85151"   # red / vermelha
 background = "#3b3b3b"
 
+# Window Configuration
 window = Tk()
 window.title("game")
 window.geometry("260x280")
 window.configure(bg=background)
+
+# Windos frames + styles
+frame_top = Frame(window, width=260, height=100, bg=black, relief="raised")
+frame_top.grid(row=0, column=0, sticky=NW)
+
+frame_bottom = Frame(window, width=260, height=300, bg=white, relief="flat")
+frame_bottom.grid(row=1, column=0, sticky=NW)
+
+style = ttk.Style(window)
+style.theme_use("clam")
 
 window.mainloop()
